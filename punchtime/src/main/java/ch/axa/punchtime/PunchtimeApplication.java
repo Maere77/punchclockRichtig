33f03c8622068d3b4861300bdc449527beddac07
AnonymUser1234 <anonym.junk@outlook.com>
2024-07-04 10:14:11 +0200

    add punchtime

---
+++ punchtime/src/main/java/ch/axa/punchtime/PunchtimeApplication.java
@@ -0,0 +1,13 @@
+package ch.axa.punchtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PunchtimeApplication.class, args);
+    }
+
+}
